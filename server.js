0dfc7e325952eca971f0b653f78e75b53c58bb50
Samir Thapa <thapasamirm577@gmail.com>
2023-10-09 20:48:54 +0545

    logger-morgan-winston

--- server.js
+++ server.js
@@ -4,9 +4,102 @@ import { sendData } from "./rabbitmq/index.js";
 import { RabbitClass } from "./rabbitmq/pubSub.js";
 import publisher from "./publisher/index.js";
 import consumer from "./consumer/index.js";
+import morgan from "morgan";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import winston from "winston";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.json({ extended: false }));
+// app.use(morgan("tiny"));
+// app.use(
+//     morgan(":method :host :url :status :res[content-length] - :response-time ms")
+// );
+
+// app.use(
+//     morgan(
+//         "combined",
+//         {
+//             skip: function (req, res) {
+//                 return res.statusCode < 400;
+//             },
+//         },
+//         function (tokens, req, res) {
+//             return [
+//                 tokens.method(req, res),
+//                 tokens.url(req, res),
+//                 tokens.status(req, res),
+//                 tokens.res(req, res, "content-length"),
+//                 "-",
+//                 tokens["response-time"](req, res),
+//                 "ms",
+//             ].join(" ");
+//         }
+//     )
+// );
+// var accessLogStream = fs.createWriteStream(path.join(__dirname, "./logs/access.log"), {
+//     flags: "a",
+// });
+
+// // setup the logger
+// app.use(morgan("combined", { stream: accessLogStream }));
+
+const logger = new winston.Logger({
+    levels: winston.config.npm.levels,
+    // format: winston.format.json(),
+    format: winston.format.combine(
+        // winston.format.colorize(),
+        // winston.format.simple(),
+        winston.format.timestamp(),
+        winston.format.printf((info) => {
+            // console.log("info", info);
+            return `${info.timestamp} ${info.level}: ${info.message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.File({
+            level: "info",
+            filename: "./logs/all-logs.log",
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false,
+        }),
+        new winston.transports.Console({
+            level: "debug",
+            handleExceptions: true,
+            // format: winston.format.simple(),
+            json: false,
+            colorize: true,
+        }),
+    ],
+    exitOnError: false,
+});
+
+// logger.stream = {
+//     write: function (message, encoding) {
+//         if (message.includes("favicon.ico")) return;
+//         // console.log("message", message);
+//         logger.info(message + "\n");
+//         // console.log("encoding", encoding);
+//         // logger.info(message);
+//         // logger.info(encoding);
+//     },
+// };
+
+app.use(
+    morgan("combined", {
+        stream: {
+            write: function (message, encoding) {
+                if (message.includes("favicon.ico")) return;
+                logger.log("info", message);
+            },
+        },
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("hello there");
@@ -73,6 +166,7 @@ app.get("/api/v1", (req, res) => {
 
 app.listen(5000, () => {
     console.log("App is running at port" + 5000);
+    return;
     connectQueue(function () {
         consumer();
         // RabbitClass.StartConsumer("test-queue", (msg, abc) => {
